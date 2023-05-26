b84aca27764625386ee38bd1e43ef3c5442ff8e6
inoi2000 <nikolaiklonov@yandex.ru>
2023-05-27 01:07:58 +0500

    Добавьте файлы проекта.

---
+++ CSharper/Models/AppConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace CSharper.Models
+{
+    public class AppConfig
+    {
+        public string ConfigurationsFolder { get; set; }
+
+        public string AppPropertiesFileName { get; set; }
+    }
+}
