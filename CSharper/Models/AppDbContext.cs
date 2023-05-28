8befffba53063f7736e3710c46370ab2775758e2
inoi2000 <nikolaiklonov@yandex.ru>
2023-05-29 00:41:39 +0500

    Добавление EF, SQLite, созданы класс сущностей

---
+++ CSharper/Models/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharper.Models
+{
+    public class AppDbContext : DbContext
+    {
+        private const string ConnectionString = "Data Source=CSharper.db";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Batteries.Init();
+            optionsBuilder.UseSqlite(ConnectionString);
+        }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Subject> Subjects => Set<Subject>();
+        public DbSet<Lesson> Lessons => Set<Lesson>();
+        public DbSet<Book> Books => Set<Book>();
+        public DbSet<Video> Videos => Set<Video>();
+        public DbSet<Article> Articles => Set<Article>();
+        public DbSet<Assignment> Assignments => Set<Assignment>();
+
+
+    }
+}
