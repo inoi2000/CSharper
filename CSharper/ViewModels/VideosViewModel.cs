9b81f0abb2cbc47741ad9b5950299612a96909de
beent <beent@PC-POWERFULWHITE>
2023-06-19 11:54:46 +0300

    Добавлена вкладка "Видео", плюс небольшие исправления опечаток

---
+++ CSharper/ViewModels/VideosViewModel.cs
@@ -0,0 +1,52 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CSharper.Models;
+using CSharper.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wpf.Ui.Common.Interfaces;
+
+namespace CSharper.ViewModels
+{
+    public partial class VideosViewModel : ObservableObject, INavigationAware
+    {
+        private VideoService _videoService;
+        private SubjectService _subjectService;
+
+        [ObservableProperty]
+        private IEnumerable<Video> _videos;
+
+        [ObservableProperty]
+        private Video selectedVideo;
+
+        [ObservableProperty]
+        private Subject selectedSubject;
+
+        [ObservableProperty]
+        private IEnumerable<Subject> _subjects;
+
+        public async void OnNavigatedTo()
+        {
+            _subjectService = new SubjectService();
+            _videoService = new VideoService();
+
+            Subjects = await _subjectService.GetAllSubjectsAcync();
+
+            if (SelectedSubject != null) { Videos = await _videoService.GetAllVideosAsync(SelectedSubject.Id); }
+            else { Videos = await _videoService.GetAllVideosAsync(); }
+        }
+
+        public void OnNavigatedFrom()
+        {
+            _videoService.Dispose();
+            _subjectService.Dispose();
+        }
+
+        public async Task UpdateVideosList()
+        {
+            if (SelectedSubject != null) Videos = await _videoService.GetAllVideosAsync(SelectedSubject.Id);
+        }
+    }
+}
---
+++ CSharper/Views/Pages/VideosPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using CSharper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf.Ui.Common.Interfaces;
+
+namespace CSharper.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для VideosPage.xaml
+    /// </summary>
+    public partial class VideosPage : INavigableView<ViewModels.VideosViewModel>
+    {
+        public ViewModels.VideosViewModel ViewModel { get; }
+
+        public VideosPage(ViewModels.VideosViewModel viewModel)
+        {
+            ViewModel = viewModel;
+
+            InitializeComponent();
+        }
+
+        private void ListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            string destinationUrl = (videosListbox.SelectedItem as Video).Url.ToString();
+            var startInfo = new System.Diagnostics.ProcessStartInfo(destinationUrl)
+            {
+                UseShellExecute = true
+            };
+            System.Diagnostics.Process.Start(startInfo);
+        }
+
+        private async void subjectsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            await ViewModel.UpdateVideosList();
+        }
+    }
+}
